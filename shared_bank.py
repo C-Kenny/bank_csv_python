44ab733a8a8735e39fe2f297779af32fe28b1080
Carl Kenny <C-Kenny@users.noreply.github.com>
2014-10-28 14:42:28 +1300

    Fixed spelling errors in comments

--- shared_bank.py
+++ shared_bank.py
@@ -13,8 +13,9 @@ The above usage assumes windows use. Linux users, you know what to do.
     
 
 def getNames():
-    """ Gets the amount of flatmates and their names, 
-    builds dictionary for reach"""
+    """ 
+    Gets the num of flatmates and their names, builds dictionary for each
+    """
     
     numberOfPeople = int(input("Please enter the number of people " +
     "living together: "))
@@ -22,7 +23,7 @@ def getNames():
     peoplesNames = dict()
     
     for i in range(numberOfPeople):
-        temp = input("Name: ")                                  #.upper()    
+        temp = input("Name: ")                                    
         peoplesNames.update({temp: [[],0]})
     
     # Adds an entry for unexplained transactions
@@ -30,93 +31,83 @@ def getNames():
     return (peoplesNames, numberOfPeople)
     
 def openCSV():
+    """ Opens .csv file as arvg 1. Returns the csv obj """
+    
     f = open(sys.argv[1], newline = '', )
     reader = csv.reader(f)
-    f.readline() # Skips first line, which is usaully the account number
+    f.readline() # Skips first line, which is usually the account number
     rowNumber = 0
     return reader
     
 def setColumns(peoplesNames, reader):
-
-        addition = []
-        rowNumber = 0
-        found = False
-           
-        # Read the first two lines to get input/output columns       
-        for row in reader:
-            if rowNumber >= 2:
-                break
-            else:
-                rowNumber += 1
-                # For each item in row, test if it's a number, i is index
-                for i in range(len(row)):
-                    if found == False:
-                        for name in peoplesNames.keys(): 
-                            # Fix as some banks use lower case names for AP's
-                            if name in row[i] or name.upper() in row[i] or name.lower() in row[i]:
-                                nameColumn = i
-                                found = True
-                    try:    
-                        float(row[i])
-                    except:
-                        pass
-                    else:
-                        addition.append([i, float(row[i])])
+    """ Determines the name column *Tested on Westpac and KiwiBank """
+    
+    addition = []
+    rowNumber = 0
+    found = False
        
-        if addition[0][1] + addition[2][1] == addition[3][1]:
-            print("YAY, input/output must be index 3")
-            return (3, nameColumn)
+    # Read the first two lines to get input/output columns       
+    for row in reader:
+        if rowNumber >= 2:
+            break
         else:
-            print("Must be other way around i.e. index 4 ")
-            return (4, nameColumn)
+            rowNumber += 1
+            # For each item in row, test if it's a number, i is index
+            for i in range(len(row)):
+                if found == False:
+                    for name in peoplesNames.keys(): 
+                        # Fix as some banks use lower case names for AP's
+                        if name in row[i] or name.upper() in row[i] or name.lower() in row[i]:
+                            nameColumn = i
+                            found = True
+                try:    
+                    float(row[i])
+                except:
+                    # TODO: Change this method of determining if a row is number
+                    pass
+                else:
+                    addition.append([i, float(row[i])])
+   
+    if addition[0][1] + addition[2][1] == addition[3][1]:
+        return (3, nameColumn)
+    else:
+        return (4, nameColumn)
     
 def getTransactions(reader, inputColumn, nameColumn, peoplesNames):
     """ Iterates through the .csv reader, returns transactions """
+    
     for row in reader:
         # Money in/Money out as adding a negative is == subtracting
         for person in peoplesNames:
-            if person in row[nameColumn] or person.lower() in row[nameColumn] or person.upper() in row[nameColumn]:
+            if person in row[nameColumn] or person.lower() in row[nameColumn] \
+                or person.upper() in row[nameColumn]:
+                    
                 # index 1 is their current total, 0 is trans list
                 peoplesNames[person][1] += float(row[inputColumn])
-                peoplesNames[person][0].append(row[nameColumn]) ###
+                peoplesNames[person][0].append(row[nameColumn])
                 break
         else:
+            # No person is found to be responsible, therefore add to unknown
             peoplesNames['Unknown'][1] += float(row[inputColumn])
-            peoplesNames['Unknown'][0].append(row[nameColumn]) ###
+            peoplesNames['Unknown'][0].append(row[nameColumn])
+            
     return peoplesNames
 
-                    
-            
-    
 def main():
     reader = openCSV()   
-    #try:
     peoplesNames, numberOfPeople = getNames()
     inputColumn, nameColumn = setColumns(peoplesNames, reader) 
     transDict = getTransactions(reader, inputColumn, nameColumn, peoplesNames)
     output =  open('output.txt', 'w')
 
-    #for key, val in transDict.items():
-        #print(key)
-        #print("Transaction List: \t", val[0])
-        #print("Ending contribution: \t", val[1], " " \
-        #+ "\n--------------------------------------------- \n")
-
-    
     for key, val in transDict.items():
         output.write(key)
         output.write("Transaction List: \t \n%s\n" % val[0])
         output.write("Ending contribution: \t \n%d " % val[1])
         output.write("\n--------------------------------------------- \n") 
-        
+    
     output.close()
-    sys.exit(0) # Exits Python
+    sys.exit(0) # Exits Python gracefully
  
 if __name__ == "__main__":
     main()
-   
-
-    
-
-
-    
